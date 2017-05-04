929ce781f6d3145681655f794839827bd4698449
Jules B <jules.bous@gmail.com>
2017-05-04 09:20:25 +0200

    add 404

---
+++ pages/404.php
@@ -0,0 +1,6 @@
+<?php echo $view->extend('layout.php') ?>
+
+<section class="section section-404 red">
+  <h1 class="title">404 - Page not found</h1>
+  <a href="/" class="back">Retour</a>
+</section>
