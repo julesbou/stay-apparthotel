e10397a60bfcb7b6f2064a3213c722b9707643ac
Jules B <jules.bous@gmail.com>
2017-05-12 13:18:43 +0200

    add link to parking

--- pages/reglement.php
+++ pages/reglement.php
@@ -17,7 +17,7 @@
 
   <h2 class="subtitle"><span>Suppléments</span></h2>
   <ul>
-    <li>Place de parking au Nouveau Siècle</li>
+    <li>Place de parking au <a target="_blank" href="https://www.google.fr/maps/place/Parking+EFFIA+Nouveau+Si%C3%A8cle+T%C3%A9l%C3%A9p%C3%A9age/@50.6371404,3.0579306,17z/data=!3m1!4b1!4m5!3m4!1s0x47c2d587ad309ef7:0xd99fc139717f482f!8m2!3d50.637137!4d3.0601246">Nouveau Siècle</a></li>
     <li>Ménage possible pour longue durée</li>
   </ul>
 
