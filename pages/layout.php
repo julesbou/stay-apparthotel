a64834a36015a2278b5d255f911160fd6ffcb6ff
Jules B <jules.bous@gmail.com>
2017-05-17 10:30:39 +0200

    add logo/favicon

--- pages/layout.php
+++ pages/layout.php
@@ -3,6 +3,7 @@
   <head>
     <link href="https://fonts.googleapis.com/css?family=Oswald|Dosis:200,300,400,500,600,700,800|Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
     <link rel="stylesheet" href="/styles.css">
+    <link rel="icon" href="/assets/favicon.ico">
     <title>Stay AppartHotel Lille</title>
     <meta name="description" content="Location d'appart-hotel sur Lille en plein centre ville.">
     <?php if (isset($view['head'])) echo $view['head']; ?>
